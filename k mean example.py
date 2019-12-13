35c6f1620a62b0913667c2370c59868d443e6321
Samir Hysa <samir.hysa.work@gmail.com>
2019-12-13 09:02:25 +0100

    added some comment to simplify the understanding

--- k mean example.py
+++ k mean example.py
@@ -5,13 +5,17 @@ from sklearn.datasets import load_digits
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-
+# load the data
 digits = load_digits()
+
+# scale the data to simplify the process
 data = scale(digits.data)
+
+# define the target and the tests
 y = digits.target
 k = len(np.unique(y))
 
-
+# create a function that will bench all the score of the algorithm
 def bench_k_means(estimator, name, data):
     estimator.fit(data)
     print('%-9s\t%i\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f'
@@ -24,6 +28,6 @@ def bench_k_means(estimator, name, data):
              metrics.silhouette_score(data, estimator.labels_,
                                       metric='euclidean')))
 
-
+# call our function and tweak the parameters
 clf = KMeans(n_clusters=k, init="random", n_init=1000)
 bench_k_means(clf, "1", data)
